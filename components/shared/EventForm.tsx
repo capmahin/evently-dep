a219708cef86a26b3e95fa2524e2fe6d54531a50
capmahin <mahinulhuque1996@gmail.com>
2024-02-10 22:29:32 +0600

    add form schema

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,6 +2,14 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
 
 type EventFormProps ={
     userId:string
