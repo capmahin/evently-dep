6e233898ec2c93de4ec7910710061db8264a4358
capmahin <mahinulhuque1996@gmail.com>
2024-02-12 02:14:06 +0600

    add slect

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -13,7 +13,16 @@ type DropdownProps={
 
 const Dropdown = ({value, onChangeHandler}: DropdownProps) => {
   return (
-    <div>Dropdown</div>
+    <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>
   )
 }
 
