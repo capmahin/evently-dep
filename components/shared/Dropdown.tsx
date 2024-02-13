b2d7e06e1f5c7444dbb8ae086b9bf5e2c156ea18
capmahin <mahinulhuque1996@gmail.com>
2024-02-13 12:29:22 +0600

    add alert dialoge

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -7,6 +7,17 @@ import {
   } from "@/components/ui/select"
 import { ICategory } from "@/lib/database/models/category.model"
 import { useState } from "react"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 type DropdownProps={
     value?: string
