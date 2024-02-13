430b3f46b32fc941cd30ab4a8cb009d2c169ddb4
capmahin <mahinulhuque1996@gmail.com>
2024-02-13 12:30:12 +0600

    add AlertDailoge logs

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -39,6 +39,23 @@ const Dropdown = ({value, onChangeHandler}: DropdownProps) => {
         {category.name}
     </SelectItem>
    ))}
+
+<AlertDialog>
+  <AlertDialogTrigger>Open</AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
   </SelectContent>
 </Select>
   )
