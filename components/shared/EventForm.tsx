c73d31671f883b6b0de5d2556d6122a72a5fa6cc
capmahin <mahinulhuque1996@gmail.com>
2024-02-20 11:47:48 +0600

    add location

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-
+import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -119,8 +119,30 @@ const EventForm = ({userId, type}: EventFormProps) => {
             />
        </div>
        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image
+                        src="/assets/icons/location-grey.svg"
+                        alt="calendar"
+                        width={24}
+                        height={24}
+                      />
 
-       </div>
+                      <Input placeholder="Event location or Online" {...field} className="input-field" />
+                    </div>
+
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        </div>
+
       
       <Button type="submit">Submit</Button>
     </form>
