8cfa2184fa97830e9bbadcfcb03896e5af0899e2
capmahin <mahinulhuque1996@gmail.com>
2024-02-24 20:26:53 +0600

    add handleerrors

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -1,10 +1,14 @@
 "use server"
 
 import { CreateCategoryParams } from "@/types"
+import { handleError } from "../utils"
+
+
+
 export const createCategory = async ({ categoryName }: CreateCategoryParams)=>{
    try {
     
    } catch (error) {
-    
+     handleError(error)
    }
 }
