5b516f904e3ae3aaef1153dd49f6580e687081fa
capmahin <mahinulhuque1996@gmail.com>
2024-02-24 20:28:57 +0600

    add newCategory

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -3,12 +3,15 @@
 import { CreateCategoryParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
+import Category from "../database/models/category.model"
 
 
 
 export const createCategory = async ({ categoryName }: CreateCategoryParams)=>{
    try {
      await connectToDatabase();
+
+     const newCategory = await Category.create({name: categoryName});
    } catch (error) {
      handleError(error)
    }
