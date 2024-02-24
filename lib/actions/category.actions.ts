8f91747be5a5aef3e94b97ec5262779dc4c5c049
capmahin <mahinulhuque1996@gmail.com>
2024-02-24 20:27:28 +0600

    connecttodatebase

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -2,12 +2,13 @@
 
 import { CreateCategoryParams } from "@/types"
 import { handleError } from "../utils"
+import { connectToDatabase } from "../database"
 
 
 
 export const createCategory = async ({ categoryName }: CreateCategoryParams)=>{
    try {
-    
+     await connectToDatabase();
    } catch (error) {
      handleError(error)
    }
