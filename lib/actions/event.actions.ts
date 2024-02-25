6b57373f66047df2f37c713e63eb1aae8c9a4b63
capmahin <mahinulhuque1996@gmail.com>
2024-02-26 02:05:01 +0600

    passthe error

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,11 +1,12 @@
 "use server"
 
 import { CreateEventParams } from "@/types"
+import { handleError } from "../utils"
 
 export const createEvent = async ({event, userId, path}: CreateEventParams)=>{
     try {
         
     } catch (error) {
-        
+        handleError(error);
     }
 }
