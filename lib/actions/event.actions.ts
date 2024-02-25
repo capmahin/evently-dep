ed85c05fc46c4175cfcddc60e686e1d37e88f104
capmahin <mahinulhuque1996@gmail.com>
2024-02-26 02:05:36 +0600

    conneecttodatabase

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -2,10 +2,11 @@
 
 import { CreateEventParams } from "@/types"
 import { handleError } from "../utils"
+import { connectToDatabase } from "../database"
 
 export const createEvent = async ({event, userId, path}: CreateEventParams)=>{
     try {
-        
+        await connectToDatabase();
     } catch (error) {
         handleError(error);
     }
