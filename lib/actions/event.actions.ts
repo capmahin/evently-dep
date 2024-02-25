f5bc22ac3a9789f952828baf9514204511a45c31
capmahin <mahinulhuque1996@gmail.com>
2024-02-26 02:06:52 +0600

    add organizer

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -3,10 +3,13 @@
 import { CreateEventParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
+import User from "../database/models/user.model"
 
 export const createEvent = async ({event, userId, path}: CreateEventParams)=>{
     try {
         await connectToDatabase();
+
+        const organizer = await User.findById(userId);
     } catch (error) {
         handleError(error);
     }
