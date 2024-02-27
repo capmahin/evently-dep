c26395c6cf01b25611c06d6a30dc5b03fb1702d6
capmahin <mahinulhuque1996@gmail.com>
2024-02-28 02:34:49 +0600

    populateit

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,14 @@ import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
 import Event from "../database/models/event.model"
+import Category from "../database/models/category.model"
+
+
+const populateEvent = (query: any) => {
+    return query
+      .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+      .populate({ path: 'category', model: Category, select: '_id name' })
+  }
 
 export const createEvent = async ({event, userId, path}: CreateEventParams)=>{
     try {
@@ -34,6 +42,8 @@ export const getEventById = async (eventId: string)=>{
         if(!event){
             throw new Error("Event not found");
         }
+
+        return JSON.parse(JSON.stringify(event));
     } catch (error) {
         handleError(error);
     }
