452e46999a1b9a14b9f0fe9d9e40a5fdf86ef3b0
capmahin <mahinulhuque1996@gmail.com>
2024-03-02 12:23:41 +0600

    add 2 div

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -18,7 +18,13 @@ const EventDetails = async({params:{id}}: SearchParamProps) => {
          <div className='flex w-full flex-col gap-8 p-5 md:p-10'>
           <div className='flex flex-col gap-6'>
              <h2 className='h2-bold'>{event.title}</h2>
+             <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
+              <div className='flex gap-3'>
+                  
+              </div>
+             </div>
           </div>
+
          </div>
        </div>
     </section>
