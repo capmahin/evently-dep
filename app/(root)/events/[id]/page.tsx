46f1d509b478c89eb0e80a9bcb34a0f9a9855e20
capmahin <mahinulhuque1996@gmail.com>
2024-03-02 12:21:21 +0600

    add h2 and rtander it

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -16,7 +16,9 @@ const EventDetails = async({params:{id}}: SearchParamProps) => {
          />
 
          <div className='flex w-full flex-col gap-8 p-5 md:p-10'>
-          
+          <div className='flex flex-col gap-6'>
+             <h2 className='h2-bold'>{event.title}</h2>
+          </div>
          </div>
        </div>
     </section>
