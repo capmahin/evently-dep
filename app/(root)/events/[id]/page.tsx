2bf7456099fa5190294503ffb4b7dca932153634
capmahin <mahinulhuque1996@gmail.com>
2024-03-02 11:49:01 +0600

    add image qith height width

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,13 +1,18 @@
 import { getEventById } from '@/lib/actions/event.actions'
 import { SearchParamProps } from '@/types'
-import React from 'react'
+import Image from 'next/image'
 
 const EventDetails = async({params:{id}}: SearchParamProps) => {
   const event = await getEventById(id)
   return (
     <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
        <div className='grid grid-cols-1 2xl:max-w-7xl'>
-
+         <Image 
+         src={event.imageUrl}
+         alt='hero image'
+         width={1000}
+         height={1000}
+         />
        </div>
     </section>
   )
