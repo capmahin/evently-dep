6d2c7311631830be127063328bdd844265d31fe2
capmahin <mahinulhuque1996@gmail.com>
2024-03-02 11:47:08 +0600

    add div withclassname

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -6,7 +6,9 @@ const EventDetails = async({params:{id}}: SearchParamProps) => {
   const event = await getEventById(id)
   return (
     <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
-       
+       <div className='grid grid-cols-1 2xl:max-w-7xl'>
+
+       </div>
     </section>
   )
 }
