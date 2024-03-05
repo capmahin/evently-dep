3db107168fdb422e6c62191402c18818984f6c28
capmahin <mahinulhuque1996@gmail.com>
2024-03-05 14:55:46 +0600

    replace it

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -4,32 +4,40 @@ import { auth } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+// import { DeleteConfirmation } from './DeleteConfirmation'
 
 type CardProps = {
-    event: IEvent,
-    hasOrderLink?: boolean,
-    hidePrice?: boolean
-  }
+  event: IEvent,
+  hasOrderLink?: boolean,
+  hidePrice?: boolean
+}
 
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
-  return (
-    <div className='group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white
-    shadow-md transition-all hover:shadow-lg md:min-h-[438px]'>
-         <Link
-          href={`/events/${event._id}`}
-          style={{backgroundImage: `url(${event.imageUrl})`}}
-          className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
-         >
-          {/* IS EVENT CREATOR ... */}
 
-          <div  className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
+  const isEventCreator = userId === event.organizer._id.toString();
+
+  return (
+    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+      <Link 
+        href={`/events/${event._id}`}
+        style={{backgroundImage: `url(${event.imageUrl})`}}
+        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+      />
+      {/* IS EVENT CREATOR ... */}
+
+      {isEventCreator && !hidePrice && (
+        <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
           <Link href={`/events/${event._id}/update`}>
             <Image src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
           </Link>
-          </div>
-          <div
+
+          {/* <DeleteConfirmation eventId={event._id} /> */}
+        </div>
+      )}
+
+      <div
         className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
       > 
        {!hidePrice && <div className="flex gap-2">
@@ -62,7 +70,6 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           )}
         </div>
       </div>
-         </Link>
     </div>
   )
 }
