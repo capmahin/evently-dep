dca415aa11712aad43abd90a28e9d9362ea93247
capmahin <mahinulhuque1996@gmail.com>
2024-03-05 15:01:06 +0600

    working on delete

---
+++ components/shared/DeleteConfirmation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
+  return (
+    <div>DeleteConfirmation</div>
+  )
+}
+
+export default DeleteConfirmation
