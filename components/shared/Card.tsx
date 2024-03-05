831e067d659c474f33d8ca5a3216127512dd883c
capmahin <mahinulhuque1996@gmail.com>
2024-03-05 14:46:28 +0600

    addf another link

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,4 +1,5 @@
 import { IEvent } from '@/lib/database/models/event.model'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -17,7 +18,10 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           style={{backgroundImage: `url(${event.imageUrl})`}}
           className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
          >
-         
+          {/* IS EVENT CREATOR ... */}
+          <Link href={`/events/${event._id}/update`}>
+            <Image src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
+          </Link>
          </Link>
     </div>
   )
