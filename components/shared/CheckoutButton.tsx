f4ae94fb0db54204bc8a4823953ff24f7feb439a
capmahin <mahinulhuque1996@gmail.com>
2024-03-07 01:18:11 +0600

    add Ievent in cheout

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,8 +1,12 @@
+"use client"
+
+
 import { IEvent } from '@/lib/database/models/event.model'
 import { SignedIn, SignedOut, useUser } from '@clerk/nextjs';
 import React from 'react'
 import { Button } from '../ui/button';
 import Link from 'next/link';
+import Checkout from './Checkout';
 
 const CheckoutButton = ({ event }: { event: IEvent })  => {
     const { user } = useUser();
@@ -23,7 +27,7 @@ const CheckoutButton = ({ event }: { event: IEvent })  => {
           </SignedOut>
 
           <SignedIn>
-            {/* <Checkout event={event} userId={userId} /> */}
+            <Checkout event={event} userId={userId}/>
           </SignedIn>
         </>
       )}
