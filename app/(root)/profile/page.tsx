6cc8ff0ece35d97c6878e8d9341887e9e6f497da
capmahin <mahinulhuque1996@gmail.com>
2024-03-06 21:50:33 +0600

    add section

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <>
+     {/* My Tickets */}
+
+     <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+
+     </section>
+
+      {/* Events Organized */}
+    </>
   )
 }
 
