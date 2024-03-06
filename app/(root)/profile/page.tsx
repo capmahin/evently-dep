d8512f451d95de37dc1acf71afcf829163f1d2ec
capmahin <mahinulhuque1996@gmail.com>
2024-03-06 21:51:44 +0600

    add another div with classname

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -6,7 +6,9 @@ const ProfilePage = () => {
      {/* My Tickets */}
 
      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+         <div className="wrapper flex items-center justify-center sm:justify-between">
 
+         </div>
      </section>
 
       {/* Events Organized */}
