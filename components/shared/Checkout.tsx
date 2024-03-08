33d617c93f198cd1e27f2471fd39d03a254602e4
capmahin <mahinulhuque1996@gmail.com>
2024-03-09 00:33:41 +0600

    replace

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
-import { Button } from '../ui/button'
-import { IEvent } from '@/lib/database/models/event.model'
 import { loadStripe } from '@stripe/stripe-js';
+
+import { IEvent } from '@/lib/database/models/event.model';
+import { Button } from '../ui/button';
+// import { checkoutOrder } from '@/lib/actions/order.actions';
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-
-const Checkout = ({event, userId}:{event: IEvent, userId:string}) => {
+const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
@@ -19,13 +20,22 @@ const Checkout = ({event, userId}:{event: IEvent, userId:string}) => {
     }
   }, []);
 
-    const onCheckout = async()=>{
-        console.log('CHECKOUT')
+  const onCheckout = async () => {
+    const order = {
+      eventTitle: event.title,
+      eventId: event._id,
+      price: event.price,
+      isFree: event.isFree,
+      buyerId: userId
     }
+
+    // await checkoutOrder(order);
+  }
+
   return (
-    <form action={onCheckout} method='post'>
-         <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-            {event.isFree ? 'Get Ticket': 'Buy Ticket'}
+    <form action={onCheckout} method="post">
+      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
+        {event.isFree ? 'Get Ticket' : 'Buy Ticket'}
       </Button>
     </form>
   )
