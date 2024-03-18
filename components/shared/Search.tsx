168a5ba3dd578144dafa058a38e8ce1b78d6ef12
capmahin <mahinulhuque1996@gmail.com>
2024-03-19 02:38:31 +0600

    add input

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -3,6 +3,7 @@
 
 import Image from 'next/image';
 import { useState } from 'react'
+import { Input } from '../ui/input';
 
 const Search = () => {
     const [query, setQuery] = useState('');
@@ -10,7 +11,9 @@ const Search = () => {
     <div className='flex-center min-h-[54px] w-full overflow-hidden rounded-full
     bg-grey-50 px-4 py-2'>
         
-      <Image src="/assets/icons/search.svg" alt='search'/>
+      <Image src="/assets/icons/search.svg" alt='search' width={24}
+      height={24}/>
+      <Input/>
     </div>
   )
 }
