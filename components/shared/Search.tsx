98d7c27b16bb933e19f38149b8d3c69423aa9d31
capmahin <mahinulhuque1996@gmail.com>
2024-03-19 02:37:26 +0600

    add Image

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react'
+"use client"
+
+
+import Image from 'next/image';
+import { useState } from 'react'
 
 const Search = () => {
     const [query, setQuery] = useState('');
   return (
-    <div>Search</div>
+    <div className='flex-center min-h-[54px] w-full overflow-hidden rounded-full
+    bg-grey-50 px-4 py-2'>
+        
+      <Image src="/assets/icons/search.svg" alt='search'/>
+    </div>
   )
 }
 
