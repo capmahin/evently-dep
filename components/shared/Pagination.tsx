587f5ee17a9e6c2ae6c8b3e02d885c43e4775c80
capmahin <mahinulhuque1996@gmail.com>
2024-05-06 14:17:49 +0600

    add anpther

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -3,6 +3,7 @@
 import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 import { Button } from '../ui/button'
+import { formUrlQuery } from '@/lib/utils'
 
 type PaginationProps ={
   page: number | string,
@@ -21,6 +22,14 @@ const Pagination = ({page, totalPages, urlParamName}:
     const pageValue = btnType === 'next' 
     ? Number(page) + 1 
     : Number(page) - 1
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || 'page',
+      value: pageValue.toString(),
+    })
+
+    router.push(newUrl, {scroll: false})
   }
   return (
     <div className='flex gap-2'>
