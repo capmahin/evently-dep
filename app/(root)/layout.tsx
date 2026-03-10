438bb7a20dcda2ec71060740887e3096ef91355c
capmahin <mahinulhuque1996@gmail.com>
2026-03-11 02:58:09 +0600

    fix: mongodb uri update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,16 +2,17 @@ import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div className=" flex h-screen w-screen flex-col">
-        {/* header */}
-        <Header/>
-        <main className="flex-1">{children}</main>
-        <Footer/>
-      </div>
-    );
-  }
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className=" flex h-screen w-screen flex-col">
+      {/* header */}
+      <Header />
+      <main className="flex-1">{children}</main>
+      {/* footer */}
+      <Footer />
+    </div>
+  );
+}
